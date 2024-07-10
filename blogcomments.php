a5cb6f2777f80cd5f1a6c528eee724d5355a3e9f
sapperwrapper <sapperwrapper@gmail.com>
2024-07-10 17:57:11 -0400

    Update blogcomments.php

--- blogcomments.php
+++ blogcomments.php
@@ -1,12 +1,10 @@
 <?php
 if (isset($_POST['email']) && !empty($_POST['email'])) {
-$subject = "New contact request: $_POST['subject']";
+$subject = $_POST['subject'];
 $message = $_POST['body'];
-$headers = 'From: info@website.com' . "\r\n" .
-           'Reply-To: ' . $_POST['email']. "\r\n" .
-           'X-Mailer: PHP/' . phpversion();
+$headers = $_POST['email'];
 
 mail('sapperwrapper@gmail.com', $subject, $message, $headers);
 
-die('Thank you for your email');
+die('Thank you for your comment');
 }
