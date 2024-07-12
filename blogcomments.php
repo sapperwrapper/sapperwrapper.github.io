b210faa2d648006977fa3e84589d9b58e4033812
sapperwrapper <sapperwrapper@gmail.com>
2024-07-12 12:42:35 -0400

    Delete blogcomments.php

--- blogcomments.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_POST['email']) && !empty($_POST['email'])) {
-$subject = $_POST['subject'];
-$message = $_POST['body'];
-$headers = $_POST['email'];
-
-mail('sapperwrapper@gmail.com', $subject, $message, $headers);
-
-}
